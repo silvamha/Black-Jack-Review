17fd1b07e796553858d9112a20efe1cc5c21a131
Ed <silva.email@yahoo.com>
2024-09-04 16:51:50 -0500

    Added CSS file

---
+++ index.js
@@ -0,0 +1,18 @@
+// 1. Create two variables, firstCard and secondCard. 
+// Set their values to a random number between 2-11
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+
+let fistCard = 6
+let secondCard = 15
+let sum = fistCard + secondCard
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+} else {
+    console.log("You're out of the game! 😭")
+}
+
+console.log(sum)
