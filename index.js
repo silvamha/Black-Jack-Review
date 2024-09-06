bca3dcb5fec144abeeaeecedd822e23b20a3ad42
Ed <silva.email@yahoo.com>
2024-09-06 12:30:39 -0500

    Add and Remove points function test

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-// 1. Create two variables, firstCard and secondCard. 
+// 1. Create two variables, firstCard and secondCard.
 // Set their values to a random number between 2-11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
-import  myPoints, {addPoints, removePoints} from "./myPoints.js";
+// import  myPoints, {addPoints, removePoints} from "./myPoints.js";
 
-let fistCard = 6
-let secondCard = 15
-let sum = fistCard + secondCard
+let fistCard = 6;
+let secondCard = 15;
+let sum = fistCard + secondCard;
 
 if (sum < 21) {
-    console.log("Do you want to draw a new card? 🙂")
+  console.log("Do you want to draw a new card? 🙂");
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
+  console.log("Wohoo! You've got Blackjack! 🥳");
 } else {
-    console.log("You're out of the game! 😭")
+  console.log("You're out of the game! 😭");
 }
 
-console.log(sum)
+console.log(sum);
 
 // let num1 = parseFloat(prompt("Enter the first number:")); // Get first number from user
 // let num2 = parseFloat(prompt("Enter the second number:")); // Get second number from user
@@ -25,7 +25,20 @@ console.log(sum)
 
 // alert("The sum is: " + sum1); // Display the result in an alert box
 
-const updatePoints = addPoints(12)
-const lowerPoints = removePoints(1)
+// const updatePoints = addPoints(12)
+// const lowerPoints = removePoints(1)
 
-console.log(updatePoints)
+// console.log(updatePoints)
+
+let myPoints = 3;
+
+const add3points = () => {
+  return (myPoints += 3);
+};
+
+const remove1Point = () => {
+  return (myPoints -= 1);
+};
+
+console.log(myPoints);
+console.log(myPoints);
