6cb2e3894dd72a3559e50ca76217a74079247aa2
Ed <silva.email@yahoo.com>
2024-09-06 16:16:07 -0500

    Corrected ordeer of operation

--- index.js
+++ index.js
@@ -54,3 +54,33 @@ console.log(typeof ("11" + "14"));
 // PEMDAS
 let myCalc = 50 + 10 / 8 + 2;
 console.log(myCalc);
+
+/*
+Challenge:
+1. The following expression should calculate total expenses 
+   for a trip, but it has a problem. It is giving me 548 but 
+   I think that is too cheap. Have a look, and see if you can 
+   figure out what problem we have with operator precedence and 
+   change the code so we log out the correct cost of the trip.
+*/
+
+const tripLengthDays = 2;
+const tripDistanceKM = 300;
+const fuelPricePerKM = 1.5;
+const hotelCostPerNight = 60;
+const hotelDiscountPerNight = 6;
+const foodCost = 50;
+
+// const totalCost = foodCost + tripDistanceKM * fuelPricePerKM + hotelCostPerNight - hotelDiscountPerNight * tripLengthDays
+
+// const totalCost = ((tripLengthDays*hotelCostPerNight)-hotelDiscountPerNight*tripLengthDays) +((tripDistanceKM*fuelPricePerKM)) + 50
+
+// console.log(totalCost)
+
+const hotelCost = (tripLengthDays*hotelCostPerNight) - (hotelDiscountPerNight*2)
+const drivingCost = (tripDistanceKM*fuelPricePerKM)
+const totalCost = hotelCost+drivingCost+foodCost
+
+console.log(`The hotel cost is  ${hotelCost}`)
+console.log(`The driving cost is ${drivingCost}`)
+console.log(`The total cost of the trip is ${totalCost}`)
