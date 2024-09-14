3ddb30f0ae1faa6347ce2bd854697ada4837a932
Ed <silva.email@yahoo.com>
2024-09-14 06:54:20 -0500

    Has Black Jack variable

--- index.js
+++ index.js
@@ -7,14 +7,25 @@
 let fistCard = 10;
 let secondCard = 11;
 let sum = fistCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let msg = ""
 
 if (sum < 21) {
   console.log("Do you want to draw a new card? 🙂");
+  msg = "Sorry, you don't have Black Jack!"
+
 } else if (sum === 21) {
   console.log("Wohoo! You've got Blackjack! 🥳");
+  hasBlackJack = true;
+  msg = "You have Black Jack!"
+
 } else {
   console.log("You're out of the game! 😭");
+  isAlive = false;
+  msg = "Sorry! You are out of the game!"
 }
 
-console.log(sum);
-
+console.log(`Do I have Black Jack? ${hasBlackJack}`);
+console.log(`Am I still on the game? ${isAlive}`)
+console.log(msg)
